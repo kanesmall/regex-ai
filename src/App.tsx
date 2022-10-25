fc18347172f0d153d035bf8abcd58e0707f1093b
kanesmall <kanesmall@outlook.com>
2022-10-25 23:46:51 +0100

    build: initial openai config

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import "./App.css"
+import { Configuration, OpenAIApi } from "openai"
+import { resourceLimits } from "worker_threads"
+const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration)
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [userInput, setUserInput] = useState<string>("")
+    const [aiRegexSuggestion, setAiRegexSuggestion] = useState<string>("")
+
+    const getAiGeneration = async () => {
+        try {
+            const response = await openai.createCompletion({
+                model: "text-davinci-002",
+                prompt: `Given the following description, output a regex string:\n\n"${userInput}"\n\nRegex:`,
+                temperature: 0.7,
+                max_tokens: 256,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 0
+            })
+
+            if (!response.data.choices?.[0].text) throw new Error("Invalid response")
+            setAiRegexSuggestion(response.data.choices?.[0].text)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
+        <>
+            <textarea
+                className=""
+                placeholder="Enter regex description"
+                onChange={(e) => setUserInput(e.target.value)}
+            />
+            <button className="" onClick={getAiGeneration}>
+                Get regex
+            </button>
+            <input type="text" value={aiRegexSuggestion} readOnly />
+        </>
     )
 }
 
